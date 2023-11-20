ed3caef67a7d2b2e6fb647dd52c528f589820849
root <funtik1229@yandex.ru>
2023-11-20 14:48:28 +0300

    bot's basics (in process)

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,20 @@
-import time
+import asyncio
+import logging
 from config import BOT_TOKEN
+from modules import handlers, callbacks, middlewares
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters.command import Command
 
-while True:
-    print(BOT_TOKEN, flush=True)
-    time.sleep(1)
+
+logging.basicConfig(level=logging.INFO)       # Logging
+bot = Bot(token=BOT_TOKEN, parse_mode="HTML") # Bot object
+dp = Dispatcher()                             # Dispatcher
+
+async def main():
+    dp.update.middleware.register(middlewares.NewUserMiddleware())  # Middleware checks if user allowed to use this bot
+    dp.message.register(handlers.start,   Command('start'))  # Handle '/start' command
+    dp.message.register(handlers.anymsg, F.text)  # Handle any text
+
+    await dp.start_polling(bot)  # Start polling
+
+asyncio.run(main())
